f5de575abc30a26ace1bb17bca5ac8dac8ad5366
Keith Cullinan <kfcullinan@gmail.com>
2022-07-04 14:49:44 -0500

    completed first function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,32 @@
 console.log('***** Music Collection *****')
 
-let Collection = [];
+let collection = [];
 
 function addToCollection(title, artist, yearPublished) {
-
+let recordObject = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
 }
 
-const album = {
-    title:"",
-    artist:"",
-    yearPublished:"",
+collection.push(recordObject);
+console.log(collection);
+return(recordObject);
 }
+
+
+    
+   
+addToCollection('Reality', 'CAmera Silens', '1983');
+addToCollection('ST', 'Adolescents', '1984');
+addToCollection('Reality', 'CAmera Silens', '1983');
+addToCollection('ST', 'Adolescents', '1984');
+addToCollection('Reality', 'CAmera Silens', '1983');
+console.log(addToCollection('ST', 'Adolescents', '1984'));
+
+function showCollection(arrayParam){
+//console.log number of items in an array array.length
+//loop over array console log each record
+//for(record of recordObject)?
+}
+showCollection(collection);
