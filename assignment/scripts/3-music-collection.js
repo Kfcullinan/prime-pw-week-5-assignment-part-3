173cbe4db2cd0242aa29898f83669b3e03f37a48
Keith Cullinan <kfcullinan@gmail.com>
2022-07-03 23:16:17 -0500

    created album  object and started to build function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+let Collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+
+}
+
+const album = {
+    title:"",
+    artist:"",
+    yearPublished:"",
+}
